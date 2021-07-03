661cbadfe5c9b5e2002d12745b01d42df3464800
Quddus1916 <quddusjunior1916@gmail.com>
2021-07-03 10:51:03 -0500

    login and sign up route

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -6,5 +6,13 @@ routes.get('/', (req, res) => {
     res.sendfile("index.html", {root: "./Templates/"});
 })
 
+routes.get('/login', (req, res) => {
+    res.sendfile("sign-in.html", {root: "./Templates/pages/"});
+})
+routes.get('/signup', (req, res) => {
+    res.sendfile("sign-up.html", {root: "./Templates/pages/"});
+})
+
+
 
 module.exports = routes;
